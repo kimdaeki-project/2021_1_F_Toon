7162164d65f344180152ddb48ff68f2f7841402f
llddoo <74532161+llddoo@users.noreply.github.com>
2021-05-26 17:47:32 +0900

    dfd

---
+++ src/main/java/com/to/t1/board/notice/NoticeService.java
@@ -0,0 +1,89 @@
+package com.to.t1.board.notice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.to.t1.board.BoardFileVO;
+import com.to.t1.board.BoardService;
+import com.to.t1.board.BoardVO;
+import com.to.t1.util.FileManager;
+import com.to.t1.util.Pager;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class NoticeService implements BoardService {
+
+	@Autowired
+	private NoticeMapper noticeMapper;
+	@Autowired
+	private FileManager fileManager;
+	
+	@Value("${board.notice.filePath}")
+	private String filePath;
+	
+	@Override
+	public List<BoardVO> getList(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		pager.makeRow();
+		Long totalCount = noticeMapper.getTotalCount(pager);
+		pager.makeNum(totalCount);
+		return noticeMapper.getList(pager);
+	}
+
+	@Override
+	public BoardVO getSelect(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		noticeMapper.setHitUpdate(boardVO);
+		return noticeMapper.getSelect(boardVO);
+	}
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public int setInsert(BoardVO boardVO, MultipartFile [] files) throws Exception {
+		// TODO Auto-generated method stub
+		int result = noticeMapper.setInsert(boardVO);
+		
+		//예외는 발생하지 않고 결과가 0이 나올경우 
+		//강제로 예외 발생
+		if(result<1) {
+			throw new Exception();
+		}
+		
+		String filePath= this.filePath;
+		
+		for(MultipartFile multipartFile:files) {
+			if(multipartFile.getSize()==0) {
+				continue;
+			}
+			String fileName= fileManager.save(multipartFile, filePath);
+			System.out.println(fileName);
+			BoardFileVO boardFileVO = new BoardFileVO();
+			boardFileVO.setFileName(fileName);
+			boardFileVO.setOriName(multipartFile.getOriginalFilename());
+			boardFileVO.setNum(boardVO.getNum());
+			noticeMapper.setFileInsert(boardFileVO);
+		}
+		
+		return result;
+	}
+
+	@Override
+	public int setUpdate(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return noticeMapper.setUpdate(boardVO);
+	}
+
+	@Override
+	public int setDelete(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return noticeMapper.setDelete(boardVO);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/to/t1/util/FileManager.java
@@ -0,0 +1,41 @@
+package com.to.t1.util;
+
+import java.io.File;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileManager {
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
+	
+	public String save(MultipartFile multipartFile, String filePath)throws Exception{
+
+		String path="static";
+		ClassPathResource classPathResource = new ClassPathResource(path);
+		File file = new File(classPathResource.getFile(), filePath);
+		
+		System.out.println(file.getAbsolutePath());
+		
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		
+		String fileName = UUID.randomUUID().toString()+"_"+multipartFile.getOriginalFilename();
+		
+		file = new File(file, fileName);
+		
+		
+		multipartFile.transferTo(file);
+		
+		return fileName;
+	}
+
+}
