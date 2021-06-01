9deaeee5eed4a5c1fe750d4f9e9d7e4c6c347b8d
OolongTea620 <rlafls9596@gmail.com>
2021-06-01 15:58:34 +0900

    testTemplate

--- src/main/java/com/to/t1/member/admin/AdminController.java
+++ src/main/java/com/to/t1/member/admin/AdminController.java
@@ -10,11 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 // Admin 기능 : 1. [회원, 작품 ]조회 2. [작가, 작품]승인 
 //3. [충전액]환불 4. Notice와  QNA관리(x) 
+	
 	//일단 접촉은 해야지
 	@GetMapping("adminPage")
 	public String adminHome(Model model) {
 		return "admin/adminPage";
 	}
-	
+	@GetMapping("test")
+	public String admintest(Model model) {
+		return "admin/test";
+	}
 	
 }
