c32257cc2552f14994858d948baabf519d12578c
OolongTea620 <rlafls9596@gmail.com>
2021-06-10 09:52:49 +0900

    Revert "Revert "Revert "Merge branch 'HyeLin_2' into main2"""
    This reverts commit dc0d43d74578f7f5a2cc588d872565f26830d500.

--- src/main/java/com/to/t1/member/admin/AdminController.java
+++ src/main/java/com/to/t1/member/admin/AdminController.java
@@ -3,7 +3,6 @@ package com.to.t1.member.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,34 +16,9 @@ public class AdminController {
 	public String adminHome(Model model) {
 		return "admin/adminPage";
 	}
-	//탬플릿 임시용 
 	@GetMapping("test")
 	public String admintest(Model model) {
 		return "admin/test";
-	}
-	
-	//웹툰 입력작업용 페이지 호출
-	@GetMapping("callToonForm")
-	public void AddToon() throws Exception {
-		
-	}
-	
-	//웹툰 정보 추가
-	@PostMapping("addNewToon")
-	public void WriteToon () throws Exception{
-		//입력 성공시 홈으로 갈 것
-	}
-	
-	//웹툰 정보 수정
-	@GetMapping("modifyToon")
-	public void updateToonInfo() throws Exception{
-		//
-	}
-
-	//웹툰 삭제..보다는 비공개로 변경  
-	@PostMapping("deleteToon")
-	public void deleteToon() throws Exception{
-		
 	}
 	
 }
--- src/test/java/com/to/t1/ticket/TicketMapperTest.java
+++
@@ -1,9 +0,0 @@
-package com.to.t1.ticket;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class TicketMapperTest {
-	
-	
-}
