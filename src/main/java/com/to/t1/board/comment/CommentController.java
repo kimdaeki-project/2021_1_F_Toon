e717b5f48a9763e5bac09565c06f32b3b2b5ec94
이석준 <llddoo@naver.com>
2021-06-26 01:02:40 +0900

    jjgkk

--- src/main/java/com/to/t1/board/comment/CommentController.java
+++ src/main/java/com/to/t1/board/comment/CommentController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.to.t1.member.MemberVO;
+
  
 @Controller
 @RequestMapping("/comment")
@@ -19,6 +21,8 @@ public class CommentController {
  
     @Autowired
     private CommentService commentService;
+    @Autowired
+    private MemberVO memberVO;
     
     
     @RequestMapping("/commentList") //댓글 리스트
@@ -30,15 +34,14 @@ public class CommentController {
     
     @RequestMapping("/insert") //댓글 작성 
     @ResponseBody
-    private long commentInsert(@RequestParam long boNum, @RequestParam String commentContents, @RequestParam String username) throws Exception {
+    private long commentInsert(@RequestParam long boNum, @RequestParam String commentContents) throws Exception {
         
         CommentVO commentVO = new CommentVO();
         commentVO.setBoNum(boNum);
         commentVO.setCommentContents(commentContents);
-        commentVO.setUsername(username);
-        int dd = commentService.commentInsert(commentVO);
-        System.out.println("나오냐"+dd);
-        return dd;
+        commentVO.setUsername("");
+        
+        return commentService.commentInsert(commentVO);
     }
     
     @RequestMapping("/update") //댓글 수정  
--- src/main/java/com/to/t1/board/comment/CommentMapper.java
+++ src/main/java/com/to/t1/board/comment/CommentMapper.java
@@ -9,19 +9,19 @@ import org.apache.ibatis.annotations.Mapper;
 public interface CommentMapper {
 
 	// 댓글 개수 int라는 데이터타입이 db 데이터가 들어올때 값이 들어오나 안들어오나 여부 판단 
-    public int commentCount() throws Exception;
+    public long commentCount() throws Exception;
  
     // 댓글 목록
     public List<CommentVO> commentList(CommentVO commentVO) throws Exception;
  
     // 댓글 작성
-    public int commentInsert(CommentVO commentVO) throws Exception;
+    public long commentInsert(CommentVO commentVO) throws Exception;
     
     // 댓글 수정
-    public int commentUpdate(CommentVO commentVO) throws Exception;
+    public long commentUpdate(CommentVO commentVO) throws Exception;
  
     // 댓글 삭제
-    public int commentDelete(long coNum) throws Exception;
+    public long commentDelete(long coNum) throws Exception;
 
 	
 }
